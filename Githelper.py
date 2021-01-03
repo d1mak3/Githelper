2407057aa053efd44fbc30dec5739fd877774f80
d1mak3 <d1mak3@yandex.ru>
2021-01-03 17:38:27 +0300

    changed line of outputs

--- Githelper.py
+++ Githelper.py
@@ -1,11 +1,19 @@
 import os
 
-path = input("Enter the path to project: ")
-if not os.path.exists(path + "/.git"):
-    response = input("Git repository doesn't exist. Create one? (Y/n): ")
-    if response == "Y" or response == "y":
-        os.system("git init")
-        print("Successfully")
-        message = input("Enter the message to commit: ")
-        link = input("Enter the link to repo: ")
-        os.system('git add . & git commit -m "' + message + '" & git push --set-upstream ' + link + ' master')
+project_path = input("Enter the path to the project: ")
+if not os.path.exists(project_path + "/.git"):
+    answer = input("There is no git repo here. Should I create one? (Y/n): ")
+    if answer == "Y" or "y":
+        message_for_commit = input("Enter the message for commit: ")
+        link_to_github = input("Enter the link to github: ")
+        os.system('git init & git add . & git commit -m "' + message_for_commit +
+                  '" & git push --set-upstream ' + link_to_github + ' master')
+else:
+    message_for_commit = input("Enter the message for commit: ")
+    os.system('git add . & git commit -m "' + message_for_commit + '"')
+    answer = input("Is your local repo connected to remote? (Y/n): ")
+    if answer == "N" or "n":
+        link_to_github = input("Enter the link to github: ")
+        os.system("git push --set-upstream " + link_to_github + " master")
+    else:
+        os.system("git push")
